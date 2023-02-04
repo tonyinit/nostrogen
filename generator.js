f5cafc430adef853a096207ca09902b8476840a9
tony <122396119+tonyinit@users.noreply.github.com>
2023-02-04 21:59:42 +0000

    [nostrogen] new features
    add paste private key fill inputs feature
    add event listener for private key input
    add words of interest (woi) feature
    search keys for words of interest if selected
    add woi checkbox
    add woi container
    add woi table
    add bech32word dictionary

--- generator.js
+++ generator.js
@@ -1,4 +1,3 @@
-
 importScripts("https://unpkg.com/nostr-tools/lib/nostr.bundle.js");
 
 let privatekey;
@@ -8,41 +7,68 @@ let npub;
 let start;
 let found = false;
 let counter = 0;
+let bech32words;
+let wordOfInterest = "";
+let searchWords;
 
-onmessage = (e) => {
-    const { prefix, isPrefix } = e.data;
-    start = Date.now();
-    while(!found) {
-      counter++;
-      // update counter UI
-      postMessage({counter: counter});
-      
-      // generate the keys
-      privatekey = NostrTools.generatePrivateKey();
-      publickey = NostrTools.getPublicKey(privatekey);
-      nsec = NostrTools.nip19.nsecEncode(privatekey);
-      npub = NostrTools.nip19.npubEncode(publickey);
+onmessage = async (e) => {
+  const { prefix, isPrefix, isSearchingWords } = e.data;
 
-      if(isMatched(prefix, npub, isPrefix)) {
-        found = true;
+  searchWords = isSearchingWords;
+  if(isSearchingWords) {
+    await fetch('bech32words.txt')
+    .then(response => response.text())
+    .then(text => {
+      bech32words = text.split('\n').filter(word => word !== '');
+    });
+  }
+
+  start = Date.now();
+  while(!found) {
+    counter++;
+    // update counter UI
+    postMessage({counter: counter});
+    
+    // generate the keys
+    privatekey = NostrTools.generatePrivateKey();
+    publickey = NostrTools.getPublicKey(privatekey);
+    nsec = NostrTools.nip19.nsecEncode(privatekey);
+    npub = NostrTools.nip19.npubEncode(publickey);
+
+    if(isMatched(prefix, npub, isPrefix)) {
+      if(wordOfInterest !== "") {
+        postMessage({wordOfInterest, npub, privatekey});
+        wordOfInterest = "";
+      } else {
         let end = Date.now();
         let time = (end - start) / 1000;
         // send the keys back to the main thread
         postMessage({ npub, nsec, publickey, privatekey, time });
       }
     }
-    
+  }
 };
 
 function isMatched(prefix, npub, isPrefix){
   if(isPrefix) {
     if (npub.substring(5, 5 + prefix.length) === prefix) {
+      found = true;
       return true;
     }
   } else {
     if (npub.substring(npub.length - prefix.length) === prefix) {
+      found = true;
       return true;
     }
   }
+  if(searchWords) {
+    for(const word of bech32words) {
+      if(npub.substring(5).startsWith(word) || npub.substring(5).endsWith(word)){
+        wordOfInterest = word;
+        return true;
+      }
+    }
+  }
+  
   return false;
 }
--- nostrogen.js
+++ nostrogen.js
@@ -1,6 +1,7 @@
 // consts
 const prefixRadioEl = document.getElementById("prefixRadio");
 const suffixRadioEl = document.getElementById("suffixRadio");
+const wordsOfInterestEl = document.getElementById("wordsOfInterestCheck");
 const prefixEl = document.getElementById("prefix");
 const progressEl = document.querySelector(".progress-bar");
 const npubEl = document.getElementById("npub");
@@ -9,10 +10,13 @@ const publicEl = document.getElementById("public");
 const privateEl = document.getElementById("private");
 const timeEl = document.getElementById("time");
 const counterEl = document.getElementById("counter");
+const tableOfInterestEl = document.getElementById("tableOfInterest");
+const wordsOfInterestContainer = document.getElementById("wordsOfInterestContainer");
 
 // lets
 let worker;
 let isPrefix = prefixRadioEl.checked;
+let isSearchingWords = wordsOfInterestEl.checked;
 
 function startWorker() {
   worker = new Worker('generator.js');
@@ -21,7 +25,10 @@ function startWorker() {
     if(e.data.counter) {
       counterEl.innerHTML = e.data.counter;
     }
-    if(e.data.npub) {
+    if(e.data.wordOfInterest) {
+      tableOfInterestEl.innerHTML += `<tr><th>${e.data.wordOfInterest}</th><td>${e.data.npub}</td><td>${e.data.privatekey}</td></tr>`
+    }
+    if(e.data.npub && !e.data.wordOfInterest) {
       const { npub, nsec, publickey, privatekey, time } = e.data;
       stopWorker();
       finishUp(npub, nsec, publickey, privatekey, time);
@@ -58,6 +65,24 @@ npubEl.addEventListener('input', (e) => {
   publicEl.value = data;
 })
 
+// input private key to fill inputs
+privateEl.addEventListener('input', (e) => {
+  publicEl.value = NostrTools.getPublicKey(privateEl.value);
+  nsecEl.value = NostrTools.nip19.nsecEncode(privateEl.value);
+  npubEl.value = NostrTools.nip19.npubEncode(publicEl.value);
+})
+
+// handle words of interest checkbox
+wordsOfInterestEl.onclick = function() {
+  if(!isSearchingWords) {
+    isSearchingWords = wordsOfInterestEl.checked;
+    wordsOfInterestContainer.style.display = "block";
+  } else {
+    isSearchingWords = wordsOfInterestEl.checked;
+    wordsOfInterestContainer.style.display = "none";
+  }
+}
+
 // start looking...
 async function generate() {
   progressEl.style="width: 100%";
@@ -67,7 +92,7 @@ async function generate() {
 
   // gather the necessary data for the worker
   const prefix = prefixEl.value;
-  const data = { prefix, isPrefix };
+  const data = { prefix, isPrefix, isSearchingWords };
   
   // start the worker
   worker.postMessage(data);
